3796c16fc47d01704096c0ca0f4fcbaa4a6c03cd
Foo Weng <41928972+limfw@users.noreply.github.com>
2025-06-19 09:09:57 +0800

    Update market_gan.py

--- model/market_gan.py
+++ model/market_gan.py
@@ -2,7 +2,12 @@ import random
 
 class MarketGAN:
     def __init__(self):
-        self.fake_signals = ["fake_breakout", "trap", "noise", "trend", "reversal"]
+        self.fake_signals = ["fake_breakout", "trap", "trend", "reversal", "noise"]
 
-    def generate_signal(self):
-        return random.choice(self.fake_signals)
+    def generate_signal(self, prediction):
+        if prediction == "buy":
+            return random.choice(["trap", "reversal"])
+        elif prediction == "sell":
+            return random.choice(["fake_breakout", "trend"])
+        else:
+            return random.choice(self.fake_signals)
